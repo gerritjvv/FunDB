413cbe6cd208e3fe9207d274aeb093df1eb279aa
gerritjvv <gerritjvv@gmail.com>
2014-04-20 23:22:11 +0200

    add table create

--- src/fundb/storage.clj
+++ src/fundb/storage.clj
@@ -12,8 +12,22 @@
        (get (dosync
              (alter databases (fn [m]
                                (if-let [stored-db (get m db-name)]
-                                 stored-db
+                                 m
                                  (assoc m db-name
                                    {:name db-name :dir (clojure.java.io/file dir) :tables {}}))))) db-name)))
 
+(defn get-database [db-name] (get @databases db-name))
 
+(defn create-table [db-name table-name dir]
+  (if-let [table (get-in @databases [db-name :tables table-name])]
+      table
+      (get-in
+         (dosync
+            (alter databases (fn [m]
+                               (if-let [table (get-in m [db-name :tables table-name])]
+                                 m
+                                 (assoc-in m [db-name :tables table-name]
+                                           {:name table-name :dir (clojure.java.io/file dir)
+                                                                   :insert-data (ref {})})))))
+
+         [db-name :tables table-name])))
--- test/fundb/storage_tests.clj
+++ test/fundb/storage_tests.clj
@@ -18,3 +18,17 @@
                      (= (clojure.java.io/file f) (:dir db))
                      (not (nil? (:tables db)))))))
 
+
+(defspec create-table-should-return-table
+  100
+  (prop/for-all [n gen/nat]
+              (let [
+                 db-name "mydb"
+                 table-name (str n)
+                 f (str "/tmp/mydbs/" db-name)
+                 table-f (str "/tmp/mydbs/mydb/" table-name)
+                 table (create-table db-name table-name table-f)]
+
+                (and (not (nil? table))
+                     (= (:name table) table-name)
+                     (= (clojure.java.io/file table-f) (:dir table))))))
