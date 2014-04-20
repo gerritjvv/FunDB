b4937b2f7fdf5a06e64de8502d2c3fac0af7894e
gerritjvv <gerritjvv@gmail.com>
2014-04-21 00:31:22 +0200

    add write data

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
  		             [fileape "0.4.4-SNAPSHOT"]
+                 [org.apache.hadoop/hadoop-common "2.2.0"]
                  [org.clojure/test.check "0.5.7" :scope "test"]
 		 [org.clojure/tools.trace "0.7.8" :scope "provided"]]
 
--- src/fundb/storage.clj
+++ src/fundb/storage.clj
@@ -1,4 +1,7 @@
-(ns fundb.storage)
+(ns fundb.storage
+  (:require [fileape.core :refer :all]
+            [fundb.veb :refer [create-root insert]])
+  (:import [java.io File DataOutputStream]))
 
 
 (def databases (ref {}))
@@ -18,6 +21,56 @@
 
 (defn get-database [db-name] (get @databases db-name))
 
+
+(defn create-ape
+  "Create ape connection that writes data for a database"
+  [dir callbacks]
+  (ape {:codec :snappy
+        :base-dir dir
+                :check-freq 1000
+                :rollover-size 536870912 ;512mb
+                :rollover-timeout 1000
+                :roll-callbacks callbacks}))
+
+(defn create-table-indexes [db-name table-name key-size]
+  (create-root key-size))
+
+(defn get-table [db-name table-name]
+  (get-in @databases [db-name :tables table-name]))
+
+
+
+(defn file-roll-callback [db-name table-name {:keys [^File file]}]
+  (prn "rollback " table-name)
+  (try
+    (if-let [table (get-table db-name table-name)]
+      (let [file-name (.getAbsolutePath file)
+            {:keys [insert-data indexes]} table
+            {:keys [file-i]} (dosync (alter
+                                      insert-data
+                                      (fn [m]
+                                        (assoc m :file-i (inc (:file-i m)) (inc (:file-i m)) []))))
+            last-file-i (dec file-i)
+            file-keys (get @insert-data last-file-i)]
+        ;for each file-key insert each key into the indexes with data :file file-name :i i
+        (if (not (empty? file-keys))
+          (let
+            [new-indexes
+              (loop [ks file-keys i 0 ind @@indexes]
+                (if-let [k (first ks)]
+                  (do
+                    (recur (rest ks) (inc i) (insert ind k {:file file-name :i i})))
+                  ind))]
+
+            (dosync
+             (alter indexes (fn [_] (delay new-indexes)))
+             (alter insert-data dissoc last-file-i)))))
+      (prn "NO TABLE FOUND"))
+    (catch Exception e (.printStackTrace e))))
+
+
+
+
 (defn create-table [db-name table-name dir]
   (if-let [table (get-in @databases [db-name :tables table-name])]
       table
@@ -28,6 +81,29 @@
                                  m
                                  (assoc-in m [db-name :tables table-name]
                                            {:name table-name :dir (clojure.java.io/file dir)
-                                                                   :insert-data (ref {})})))))
+                                            :indexes (ref (delay (create-table-indexes db-name table-name Long/MAX_VALUE)))
+                                            :ape (delay (create-ape dir [(partial file-roll-callback db-name table-name)]))
+                                            :insert-data (ref {:file-i 0 0 [] })})))))
 
          [db-name :tables table-name])))
+
+
+(defn write-table [db-name table-name k ^"[B" bts]
+  ;write data
+  (let [{:keys [insert-data ape]} (get-in @databases [db-name :tables table-name])]
+    (write @ape "data"
+           (fn [^DataOutputStream out]
+             (.write out (count bts))
+             (.write out bts 0 (count bts))))
+    (dosync
+     (alter insert-data
+            (fn [data]
+              ;file-i points to a vector, this vector contains the current keys to be inserted into the index
+              ;this delay to index writing is done because we only know the file name of the data once it has been rolled.
+              (let [file-i (:file-i data)
+                    data-vec (get data file-i)]
+                (assoc data file-i (conj data-vec k))))))))
+
+
+
+
